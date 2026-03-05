035cabfd686ca04f6b9d8e31072ac16bed60e194
Mika Aßmus <mika.assmus@gmail.com>
2026-03-05 21:07:40 +0100

    Update Mesh and Meshing

--- src/mesh.rs
+++ src/mesh.rs
@@ -1,4 +1,4 @@
-use glam::IVec3;
+use glam::{IVec3, UVec3};
 use std::ops;
 
 use crate::engine::LodLevel;
@@ -7,13 +7,12 @@ pub type TextureID = u16;
 
 /// The kind states the orientation and the texture.
 /// It has the following layout:
-///                             LODs|                        texture|
+/// ORIENT| LOD |x x x x x y y y y y z z z z z| texture
 /// |0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|0|
 ///
 #[repr(C)]
 #[derive(Debug, Copy, Clone)]
 pub struct Instance {
-    pub pos: IVec3,
     pub kind: u32,
 }
 unsafe impl bytemuck::Pod for Instance {}
@@ -28,44 +27,23 @@ impl Instance {
             // This means that our shaders will only change to use the next
             // instance when the shader starts processing a new instance
             step_mode: wgpu::VertexStepMode::Instance,
-            attributes: &[
-                // A mat4 takes up 4 vertex slots as it is technically 4 vec4s. We need to define a slot
-                // for each vec4. We'll have to reassemble the mat4 in the shader.
-                wgpu::VertexAttribute {
-                    offset: 0,
-                    // While our vertex shader only uses locations 0, and 1 now, in later tutorials, we'll
-                    // be using 2, 3, and 4, for Vertex. We'll start at slot 5, not conflict with them later
-                    shader_location: 2,
-                    format: wgpu::VertexFormat::Sint32x3,
-                },
-                wgpu::VertexAttribute {
-                    offset: mem::size_of::<IVec3>() as wgpu::BufferAddress,
-                    shader_location: 3,
-                    format: wgpu::VertexFormat::Uint32,
-                },
-            ],
+            attributes: &[wgpu::VertexAttribute {
+                offset: mem::size_of::<IVec3>() as wgpu::BufferAddress,
+                shader_location: 3,
+                format: wgpu::VertexFormat::Uint32,
+            }],
         }
     }
 }
 
 #[derive(Debug, Clone)]
 pub struct Mesh {
-    pub nx: Vec<Instance>,
-    pub px: Vec<Instance>,
-    pub ny: Vec<Instance>,
-    pub py: Vec<Instance>,
-    pub nz: Vec<Instance>,
-    pub pz: Vec<Instance>,
+    pub buf: Vec<Instance>,
 }
 
 impl ops::AddAssign<Self> for Mesh {
     fn add_assign(&mut self, mut other: Self) {
-        self.nx.append(&mut other.nx);
-        self.px.append(&mut other.px);
-        self.ny.append(&mut other.ny);
-        self.py.append(&mut other.py);
-        self.nz.append(&mut other.nz);
-        self.pz.append(&mut other.pz);
+        self.buf.append(&mut other.buf);
     }
 }
 
@@ -73,88 +51,88 @@ impl ops::Add for Mesh {
     type Output = Self;
 
     fn add(mut self, mut other: Self) -> Self {
-        self.nx.append(&mut other.nx);
-        self.px.append(&mut other.px);
-        self.ny.append(&mut other.ny);
-        self.py.append(&mut other.py);
-        self.nz.append(&mut other.nz);
-        self.pz.append(&mut other.pz);
+        self.buf.append(&mut other.buf);
         self
     }
 }
 
 impl Mesh {
     pub fn new() -> Self {
-        Self {
-            nx: vec![],
-            px: vec![],
-            ny: vec![],
-            py: vec![],
-            nz: vec![],
-            pz: vec![],
-        }
+        Self { buf: vec![] }
     }
 
     pub fn with_capacity(capacity: usize) -> Self {
         Self {
-            nx: Vec::with_capacity(capacity),
-            px: Vec::with_capacity(capacity),
-            ny: Vec::with_capacity(capacity),
-            py: Vec::with_capacity(capacity),
-            nz: Vec::with_capacity(capacity),
-            pz: Vec::with_capacity(capacity),
+            buf: Vec::with_capacity(capacity),
         }
     }
 
     pub fn len(&self) -> usize {
-        self.nx.len()
-            + self.nx.len()
-            + self.px.len()
-            + self.ny.len()
-            + self.py.len()
-            + self.nz.len()
-            + self.pz.len()
+        self.buf.len()
     }
 
-    pub fn add_nx(&mut self, pos: IVec3, texture: TextureID, lod: LodLevel) {
-        self.nx.push(Instance {
-            pos,
-            kind: ((lod as u32) << 16) | texture as u32,
+    pub fn add_nx(&mut self, pos: UVec3, texture: TextureID, lod: LodLevel) {
+        self.buf.push(Instance {
+            kind: ((lod as u32) << 26)
+                | (pos.x << 21)
+                | (pos.y << 16)
+                | (pos.z << 11)
+                | texture as u32,
         });
     }
 
-    pub fn add_px(&mut self, pos: IVec3, texture: TextureID, lod: LodLevel) {
-        self.px.push(Instance {
-            pos,
-            kind: ((lod as u32) << 16) | texture as u32,
+    pub fn add_px(&mut self, pos: UVec3, texture: TextureID, lod: LodLevel) {
+        self.buf.push(Instance {
+            kind: (0b001 << 29)
+                | ((lod as u32) << 26)
+                | (pos.x << 21)
+                | (pos.y << 16)
+                | (pos.z << 11)
+                | texture as u32,
         });
     }
 
-    pub fn add_ny(&mut self, pos: IVec3, texture: TextureID, lod: LodLevel) {
-        self.ny.push(Instance {
-            pos,
-            kind: ((lod as u32) << 16) | texture as u32,
+    pub fn add_ny(&mut self, pos: UVec3, texture: TextureID, lod: LodLevel) {
+        self.buf.push(Instance {
+            kind: (0b010 << 29)
+                | ((lod as u32) << 26)
+                | (pos.x << 21)
+                | (pos.y << 16)
+                | (pos.z << 11)
+                | texture as u32,
         });
     }
 
-    pub fn add_py(&mut self, pos: IVec3, texture: TextureID, lod: LodLevel) {
-        self.py.push(Instance {
-            pos,
-            kind: ((lod as u32) << 16) | texture as u32,
+    pub fn add_py(&mut self, pos: UVec3, texture: TextureID, lod: LodLevel) {
+        self.buf.push(Instance {
+            kind: (0b011 << 29)
+                | ((lod as u32) << 26)
+                | (pos.x << 21)
+                | (pos.y << 16)
+                | (pos.z << 11)
+                | texture as u32,
         });
     }
 
-    pub fn add_nz(&mut self, pos: IVec3, texture: TextureID, lod: LodLevel) {
-        self.nz.push(Instance {
-            pos,
-            kind: ((lod as u32) << 16) | texture as u32,
+    pub fn add_nz(&mut self, pos: UVec3, texture: TextureID, lod: LodLevel) {
+        self.buf.push(Instance {
+            kind: (0b100 << 29)
+                | ((lod as u32) << 26)
+                | (pos.x << 21)
+                | (pos.y << 16)
+                | (pos.z << 11)
+                | texture as u32,
         });
     }
 
-    pub fn add_pz(&mut self, pos: IVec3, texture: TextureID, lod: LodLevel) {
-        self.pz.push(Instance {
-            pos,
-            kind: ((lod as u32) << 16) | texture as u32,
+    pub fn add_pz(&mut self, pos: UVec3, texture: TextureID, lod: LodLevel) {
+        self.buf.push(Instance {
+            kind: (0b101 << 29)
+                | ((lod as u32) << 26)
+                | (pos.x << 21)
+                | (pos.y << 16)
+                | (pos.z << 11)
+                | texture as u32,
         });
     }
 }
--- src/meshing.rs
+++ src/meshing.rs
@@ -29,54 +29,6 @@ pub fn get_axis_aligned_solid_maps(data: &DenseChunk) -> [BitMap3D; 3] {
     [x_aligned, y_aligned, z_aligned]
 }
 
-fn get_x_aligned_solid_map(data: &DenseChunk) -> BitMap3D {
-    let mut x_aligned = [[0; 32]; 32];
-
-    // data setup
-    for (i, voxel) in data.iter().enumerate() {
-        let UVec3 { x, y, z } = idx_to_coord(i);
-
-        let voxel_is_solid_u32 = voxel::is_solid_u32(*voxel);
-
-        if voxel_is_solid_u32 > 0 {
-            x_aligned[y as usize][z as usize] |= voxel_is_solid_u32 >> x;
-        }
-    }
-    x_aligned
-}
-
-fn get_y_aligned_solid_map(data: &DenseChunk) -> BitMap3D {
-    let mut y_aligned = [[0; 32]; 32];
-
-    // data setup
-    for (i, voxel) in data.iter().enumerate() {
-        let UVec3 { x, y, z } = idx_to_coord(i);
-
-        let voxel_is_solid_u32 = voxel::is_solid_u32(*voxel);
-
-        if voxel_is_solid_u32 > 0 {
-            y_aligned[z as usize][x as usize] |= voxel_is_solid_u32 >> y;
-        }
-    }
-    y_aligned
-}
-
-fn get_z_aligned_collider(data: &DenseChunk) -> BitMap3D {
-    let mut z_aligned = [[0; 32]; 32];
-
-    // data setup
-    for (i, voxel) in data.iter().enumerate() {
-        let UVec3 { x, y, z } = idx_to_coord(i);
-
-        let voxel_is_solid_u32 = voxel::is_physically_solid_u32(*voxel);
-
-        if voxel_is_solid_u32 > 0 {
-            z_aligned[x as usize][y as usize] |= voxel_is_solid_u32 >> z;
-        }
-    }
-    z_aligned
-}
-
 /// 0 = -x
 /// 1 = +x
 /// 2 = -y
@@ -104,37 +56,34 @@ pub fn map_visible(
 const FIRST_BIT: u32 = 0b1000_0000_0000_0000_0000_0000_0000_0000;
 
 pub fn generate_mesh(chunk: ChunkID, data: &DenseChunk, faces: [BitMap3D; 6]) -> Mesh {
-    let chunk_pos = chunk.pos << 5;
     let mut mesh = Mesh::with_capacity(100);
     for x in 0..32_usize {
         for y in 0..32_usize {
             for z in 0..32_usize {
-                let voxel = data[coords_to_1d_index(UVec3::new(x as u32, y as u32, z as u32))];
+                let pos = UVec3::new(x as u32, y as u32, z as u32);
+                let voxel = data[coords_to_1d_index(pos)];
 
                 if voxel == VoxelTypes::Air as u16 {
                     continue;
                 }
 
-                let position: IVec3 =
-                    (chunk_pos + IVec3::new(x as i32, y as i32, z as i32)) << chunk.lod;
-
                 if faces[0][y][z] & (FIRST_BIT >> x) != 0 {
-                    mesh.add_nx(position, voxel::texture_id(voxel, 0), chunk.lod)
+                    mesh.add_nx(pos, voxel::texture_id(voxel, 0), chunk.lod)
                 }
                 if faces[1][y][z] & (FIRST_BIT >> x) != 0 {
-                    mesh.add_px(position, voxel::texture_id(voxel, 1), chunk.lod)
+                    mesh.add_px(pos, voxel::texture_id(voxel, 1), chunk.lod)
                 }
                 if faces[2][z][x] & (FIRST_BIT >> y) != 0 {
-                    mesh.add_ny(position, voxel::texture_id(voxel, 2), chunk.lod)
+                    mesh.add_ny(pos, voxel::texture_id(voxel, 2), chunk.lod)
                 }
                 if faces[3][z][x] & (FIRST_BIT >> y) != 0 {
-                    mesh.add_py(position, voxel::texture_id(voxel, 3), chunk.lod)
+                    mesh.add_py(pos, voxel::texture_id(voxel, 3), chunk.lod)
                 }
                 if faces[4][x][y] & (FIRST_BIT >> z) != 0 {
-                    mesh.add_nz(position, voxel::texture_id(voxel, 4), chunk.lod)
+                    mesh.add_nz(pos, voxel::texture_id(voxel, 4), chunk.lod)
                 }
                 if faces[5][x][y] & (FIRST_BIT >> z) != 0 {
-                    mesh.add_pz(position, voxel::texture_id(voxel, 5), chunk.lod)
+                    mesh.add_pz(pos, voxel::texture_id(voxel, 5), chunk.lod)
                 }
             }
         }
